1ddb6d760024490495f2cb92a022f92737ae23c7
a.m.student <tailyrdebose2017@gmail.com>
2021-01-20 10:05:14 -0600

    taco

---
+++ Present/Present/PresentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentApp.swift
+//  Present
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PresentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
